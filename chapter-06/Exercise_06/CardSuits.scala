8bd809637e5ca418ca4521c2720e8bff879a2b6e
Pierre Enoc <pierre_enoc@yahoo.fr>
2019-07-12 18:03:53 -0300

    add: solution for the exercise-6

---
+++ chapter-06/Exercise_06/CardSuits.scala
@@ -0,0 +1,11 @@
+package Exercise_06
+
+object CardSuits extends Enumeration {
+
+   type CardSuits = Value
+   val Club = Value("♣")
+   val Diamond = Value("♦")
+   val Heart = Value("♥")
+   val Spade = Value("♠")
+
+}
