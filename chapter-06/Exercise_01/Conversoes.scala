6af80d3ffb540ca070359001deaa81aff4e07b92
Pierre Enoc <pierre_enoc@yahoo.fr>
2019-07-12 14:32:39 -0300

    add: soltion for exercise-1

---
+++ chapter-06/Exercise_01/Conversoes.scala
@@ -0,0 +1,8 @@
+package Exercise_01
+
+object Conversoes extends App {
+  def inchesToCentimeters(inches: Double) = inches * 2.54
+  def gallonsToLiters(gallons: Double)  = gallons * 3.78
+  def milesToKilometers(miles: Double) = miles * 1.60
+
+}
