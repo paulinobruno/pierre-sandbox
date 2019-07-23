97d1b2a7ca99746cb6f457016e4860d8295da7ed
Pierre Enoc <pierre_enoc@yahoo.fr>
2019-07-23 13:47:52 -0300

    fix: identation

--- chapter-06/Exercise_01/Conversoes.scala
+++ chapter-06/Exercise_01/Conversoes.scala
@@ -5,8 +5,8 @@ milesToKilometers .*/
 package Exercise_01
 
 object Conversoes extends App {
-  def InchesToCentimeters(inches: Double) = inches * 2.54
-  def GallonsToLiters(gallons: Double)  = gallons * 3.78
-  def MilesToKilometers(miles: Double) = miles * 1.60
+  def inchesToCentimeters(inches: Double) = inches * 2.54
+  def gallonsToLiters(gallons: Double)  = gallons * 3.78
+  def milesToKilometers(miles: Double) = miles * 1.60
 
 }
