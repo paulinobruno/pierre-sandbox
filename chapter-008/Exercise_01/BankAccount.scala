9025d48e9f200f95d816d0654ae80880831e59d1
pierreEnoc <pierre_enoc@yahoo.fr>
2019-09-12 10:22:18 -0300

    add: solution 1-2 for chapter-08

---
+++ chapter-008/Exercise_01/BankAccount.scala
@@ -0,0 +1,31 @@
+/**
+1. Extend the following BankAccount class to a CheckingAccount class that
+   charges $1 for every deposit and withdrawal.
+  */
+
+package Exercise_01
+
+class BankAccount(initialBalance: Double) {
+  private var balance = initialBalance
+    def deposit(amount: Double) = {
+      balance += amount;
+      balance
+  }
+  def withdraw(amount: Double):Double= {
+    balance -= amount;
+    balance
+
+  }
+}
+
+class CheckingAccount(initialBalance:Double) extends BankAccount(initialBalance){
+
+ private val charge : Double = 1
+
+  override def deposit(amount: Double)= {super.deposit(amount - 1)}
+
+  override def withdraw(amount: Double)= {super.withdraw(amount + 1)}
+
+}
+
+
